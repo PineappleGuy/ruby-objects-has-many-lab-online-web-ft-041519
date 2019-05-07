e1a4048b8c56b4fcefeab95249ea3384db328db1
Garth Lilly <garff247@gmail.com>
2019-05-07 20:26:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,23 @@
 class Author
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @post << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+  end
 end
