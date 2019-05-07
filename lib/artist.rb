c5b3ece95cdd91b06f33ee474611954338d42105
Garth Lilly <garff247@gmail.com>
2019-05-07 18:09:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,24 @@
 class Artist
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+  
+  def add_song(song)
+    @song << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+  
 end
